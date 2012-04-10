b7547597654908c4f89004d91d48723a91c138ff
Secretariat1987 <mixey.viti@bigmir.net>
2012-04-09 17:31:48 -0700

    function prototypes added

---
+++ fmon.rb
@@ -0,0 +1,13 @@
+require 'net/smtp'  #for sending mail tp specified adress
+
+def read_set( "setting.conf")
+
+end
+
+def file_monitor( input_dir )
+
+end
+
+def send_mail( fname, fsize, ftime )
+
+end
